edfd1f1751a03cab6e17016891a8626ac59f83f1
Abhishek Mishra <abhishekbnm79@gmail.com>
2025-11-21 00:45:53 +0530

    code

---
+++ alphabat.py
@@ -0,0 +1,12 @@
+ch=input("enter a character :")
+if ('a' <= ch <='z') :
+    print(ch , "is alphabat")
+else :
+    print(ch,"not alphabat")
+
+
+    ch=input("enter a character :")
+if ch.isalpha() :
+    print(ch , "is alphabat")
+else :
+    print(ch,"not alphabat")
